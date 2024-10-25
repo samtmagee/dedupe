c52f1d2963e1ee9199ca37eb8745bc77f6a16011
Sam Magee <sam_t_magee@hotmail.com>
2024-10-26 08:28:52 +1300

    Updated data path

--- Dedupe_TwoFolderTreewithCSV.ps1
+++ Dedupe_TwoFolderTreewithCSV.ps1
@@ -1,14 +1,14 @@
 # Define folders to compare, folder 1 is the folder we want to prune file from that already exist in folder 2
-$folder1 = "$env:USERPROFILE\github\dedupe\Test Data\Duplicates in two folders with csv\Test data\Folder 1"
-$folder2 = "$env:USERPROFILE\github\dedupe\Test Data\Duplicates in two folders with csv\Test data\Folder 2"
+$folder1 = "$env:USERPROFILE\OneDrive\GITHub\dedupe\Test Data\Duplicates in two folders with csv\Test data\Folder 1"
+$folder2 = "$env:USERPROFILE\OneDrive\GITHub\dedupe\Test Data\Duplicates in two folders with csv\Test data\Folder 2"
 
 # Get a list of the files, hash them and store in a CSV file with their algorithm , hash and path
-Get-ChildItem -Path $folder1 -Recurse | Get-FileHash | Export-Csv -Path "$env:USERPROFILE\github\dedupe\Test Data\Duplicates in two folders with csv\folder1.csv" -NoTypeInformation
-Get-ChildItem -Path $folder2 -Recurse | Get-FileHash | Export-Csv -Path "$env:USERPROFILE\github\dedupe\Test Data\Duplicates in two folders with csv\folder2.csv" -NoTypeInformation
+Get-ChildItem -Path $folder1 -Recurse | Get-FileHash | Export-Csv -Path "$env:USERPROFILE\OneDrive\GITHub\dedupe\Test Data\Duplicates in two folders with csv\folder1.csv" -NoTypeInformation
+Get-ChildItem -Path $folder2 -Recurse | Get-FileHash | Export-Csv -Path "$env:USERPROFILE\OneDrive\GITHub\dedupe\Test Data\Duplicates in two folders with csv\folder2.csv" -NoTypeInformation
 
 # Import the CSV files
-$data1 = Import-Csv -Path "$env:USERPROFILE\github\dedupe\Test Data\Duplicates in two folders with csv\folder1.csv"
-$data2 = Import-Csv -Path "$env:USERPROFILE\github\dedupe\Test Data\Duplicates in two folders with csv\folder2.csv"
+$data1 = Import-Csv -Path "$env:USERPROFILE\OneDrive\GITHub\dedupe\Test Data\Duplicates in two folders with csv\folder1.csv"
+$data2 = Import-Csv -Path "$env:USERPROFILE\OneDrive\GITHub\dedupe\Test Data\Duplicates in two folders with csv\folder2.csv"
 
 # Compare the CSV files and return hashes that exist in both CSV files
 $comparison = Compare-Object -ReferenceObject $data1.'Hash' -DifferenceObject $data2.'Hash' -ExcludeDifferent -IncludeEqual
