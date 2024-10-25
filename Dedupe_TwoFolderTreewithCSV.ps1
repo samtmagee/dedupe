64719f3b202e062bb111c78e538acd47eba81117
Sam Magee <sam_t_magee@hotmail.com>
2024-10-26 08:44:47 +1300

    Changed test data folder names

--- Dedupe_TwoFolderTreewithCSV.ps1
+++ Dedupe_TwoFolderTreewithCSV.ps1
@@ -1,6 +1,6 @@
 # Define folders to compare, folder 1 is the folder we want to prune file from that already exist in folder 2
-$folderA = "$env:USERPROFILE\OneDrive\GITHub\Dedupe\Test Data\Duplicates in two folders with csv\Test data\Folder 1"
-$folderB = "$env:USERPROFILE\OneDrive\GITHub\Dedupe\Test Data\Duplicates in two folders with csv\Test data\Folder 2"
+$folderA = "$env:USERPROFILE\OneDrive\GITHub\Dedupe\Test Data\Duplicates in two folders with csv\Test data\Folder A"
+$folderB = "$env:USERPROFILE\OneDrive\GITHub\Dedupe\Test Data\Duplicates in two folders with csv\Test data\Folder B"
 
 # Get a list of the files, hash them and store in a CSV file with their algorithm , hash and path
 $folderAData = Get-ChildItem -Path $folderA -Recurse | Get-FileHash
