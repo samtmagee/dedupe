3b9913ace59e039d8a10cfb4c285dd272d663007
Sam Magee <sam_t_magee@hotmail.com>
2024-10-26 08:54:29 +1300

    Updated comments

--- Dedupe_TwoFolderTreewithCSV.ps1
+++ Dedupe_TwoFolderTreewithCSV.ps1
@@ -1,17 +1,17 @@
-# Define folders to compare, folder 1 is the folder we want to prune file from that already exist in folder 2
+# Define folders to compare, folderB is the folder we want to prune file from that already exist in folderA
 $folderA = "$env:USERPROFILE\OneDrive\GITHub\Dedupe\Test Data\Duplicates in two folders with csv\Test data\Folder A"
 $folderB = "$env:USERPROFILE\OneDrive\GITHub\Dedupe\Test Data\Duplicates in two folders with csv\Test data\Folder B"
 
-# Get a list of the files, hash them and store in a CSV file with their algorithm , hash and path
+# Get a list of the files, hash them and store in a variable with their algorithm, hash and path
 $folderAData = Get-ChildItem -Path $folderA -Recurse | Get-FileHash
 $folderBData = Get-ChildItem -Path $folderB -Recurse | Get-FileHash
 
-# Export the CSV files
+# Export data to CSV files (these files are not used in code, just for your reference)
 $folderAData | Export-Csv -Path "$env:USERPROFILE\Dedupe - FolderA.csv" -NoTypeInformation
 $folderBData | Export-Csv -Path "$env:USERPROFILE\Dedupe - FolderB.csv" -NoTypeInformation
 
 # Compare the CSV files and return hashes that exist in both CSV files
 $comparison = Compare-Object -ReferenceObject $folderAData.'Hash' -DifferenceObject $folderBData.'Hash' -ExcludeDifferent -IncludeEqual
 
-# Show Algorithm, Hash and Path from the comparison that show up in folder 1
+# Show Algorithm, Hash and Path from the comparison that show up in folderB
 $folderBData | Where-Object { $comparison.'InputObject' -contains $_.'Hash' } | Format-Table -AutoSize Algorithm,Hash,Path
